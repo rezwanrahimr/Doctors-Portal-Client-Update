ec171074250e04fe2de7f45194a20b51b70564ba
rezwanrahimr <rezwanrahim69@gmail.com>
2023-05-29 16:01:12 +0600

    added payment method

--- src/Pages/Dashboard/CheckOutForm/CheckOutForm.jsx
+++ src/Pages/Dashboard/CheckOutForm/CheckOutForm.jsx
@@ -1,38 +1,104 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import Loader from "../../Shared/Loader/Loader";
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ booking }) => {
+  const [cardError, setCardError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [processing, setProcessing] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [booking]);
+
+  if (processing) {
+    return <Loader></Loader>;
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!stripe || !elements) {
       return;
     }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setCardError(error.message);
+    } else {
+      setCardError("");
+    }
+
+    setProcessing(true);
+    const { paymentIntent, error: pamentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: booking?.name,
+            email: booking?.email,
+          },
+        },
+      });
+    if (pamentError) {
+      cardError(pamentError.message);
+      return;
+    }
+    if (paymentIntent.status === "succeeded") {
+      setProcessing(false);
+    }
+    console.log(paymentIntent);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button
+          className="btn btn-primary"
+          type="submit"
+          disabled={!stripe || !clientSecret}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-500">{cardError}</p>
+    </>
   );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
 import { useLoaderData } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "../CheckOutForm/CheckOutForm";
 
 const Payment = () => {
   const { user } = useContext(AuthContext);
@@ -11,6 +13,7 @@ const Payment = () => {
   const stripePromise = loadStripe(`${import.meta.env.VITE_STRIPE_PK}`);
   const { appointmentDate, email, patientName, slots, treatmentName, price } =
     booking;
+
   return (
     <div>
       <div>
@@ -20,6 +23,11 @@ const Payment = () => {
           Your Appointment:{appointmentDate} at {slots}
         </p>
         <p>Please Pay: ${price}</p>
+      </div>
+      <div className="w-96">
+        <Elements stripe={stripePromise}>
+          <CheckOutForm booking={booking}></CheckOutForm>
+        </Elements>
       </div>
     </div>
   );
